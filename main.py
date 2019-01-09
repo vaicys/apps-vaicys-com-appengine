059d39df19d6c66861fc616a02039e4f6d7059b8
Vytautas Vaičys <vytautas@vaicys.com>
2019-01-09 20:03:26 +0100

    Initial source code commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('main.html')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
